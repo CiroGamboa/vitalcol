89abf60057cf895d09f16c484c729a7a333b784f
Sergioc013 <sergio013mar@gmail.com>
2020-05-30 13:15:50 -0500

    Camera Screen

--- vitalcol2020/lib/main.dart
+++ vitalcol2020/lib/main.dart
@@ -16,12 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      initialRoute: '/camera',
       routes: routes,
     );
   }
--- vitalcol2020/lib/screens/cameraScreen.dart
+++ vitalcol2020/lib/screens/cameraScreen.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -6,8 +8,101 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+
+  File imageFile;
+  final picker = ImagePicker();
+
+  Future openGallery() async {
+    final picture= await picker.getImage(source: ImageSource.gallery);
+    this.setState((){
+      imageFile=File(picture.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future openCamera() async {
+    final picture= await picker.getImage(source: ImageSource.camera);
+    this.setState((){
+      imageFile=File(picture.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Seleccione"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text("Galeria"),
+                    onTap: () {
+                      openGallery();
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(8.0),),
+                  GestureDetector(
+                    child: Text("Cámara"),
+                    onTap: () {
+                      openCamera();
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+  //retorna  texto si no se ha seleccionado alguna imagen
+  Widget _decideImageView(){
+    if(imageFile==null){
+      return Text("No se ha seleccionado ninguna imagen",
+      style: TextStyle(color:Colors.white, fontSize: 15),);
+    }
+    else{
+      return  Image.file(
+        imageFile,
+        width: MediaQuery.of(context).size.width, 
+        height: MediaQuery.of(context).size.height-200);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Cámara"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [const Color(0xFF319966), const Color(0xFF2F9BC5)],
+          begin: FractionalOffset.topLeft,
+          end: FractionalOffset.bottomRight,
+          stops: [0.0,1.0],
+          tileMode: TileMode.clamp)
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _decideImageView(),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusDirectional.circular(10)
+                ),
+                onPressed: () {
+                  _showChoiceDialog(context);
+                },
+                child: Text("Selecciona la imagen"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
