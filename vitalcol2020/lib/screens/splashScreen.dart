91d77a8e1e3c1ab70984118bd09157194f83a4fd
Jairo Sanchez <jairosr_1995@hotmail.com>
2020-05-30 11:33:18 -0500

    build.gradles configurados para la integracion de firebase en la aplicacion

--- vitalcol2020/lib/screens/splashScreen.dart
+++ vitalcol2020/lib/screens/splashScreen.dart
@@ -1,11 +1,59 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+  AnimationController animationController;
+  Animation<double> animation;
+
+  startTimer() {
+    var duration = new Duration(seconds: 3);
+    return new Timer(duration, navigatorPage);
+  }
+
+  navigatorPage() {
+    Navigator.pushNamed(context, 'login');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 1,
+      ),
+    );
+    animation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeInOut,
+    );
+    animationController.forward();
+    startTimer();
+  }
+
+  @override
+  dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
