12ecb9b3f9ed3c09f8bb3e54945700df39ed76c9
Jhoan Felipe Sarmiento Ortiz <37603383+FelipeSarmiento98@users.noreply.github.com>
2020-05-30 15:27:15 -0500

    Gestion BD desde Python

---
+++ vitalcol2020/lib/FirebasePython/GestionFirebase.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 09:49:00 2020
+
+@author: Felipe Sarmiento
+"""
+#pip install firebase-admin google-cloud-firestore google-cloud-storage
+import google.cloud
+from google.cloud import storage
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+#Cargar ruta certificado
+#ruta_certificado="C:/Users/Felipe Sarmiento/Downloads/vitalcol-firebase-adminsdk-ys044-dbc2c8d81d.json"
+#cred = credentials.Certificate(ruta_certificado)
+
+#Correr una sola vez en caso de que las funciones no sirvan, luego volver a comentar
+'''firebase_admin.initialize_app(cred,{
+            'databaseURL': 'https://{}.firebaseio.com'.format("vitalcol"),
+            'storageBucket': 'vitalcol.appspot.com'.format("vitalcol")}, name='vitalCol')'''
+
+
+collection=u'test'
+
+def consultarFirestoreBD(collection):
+    store = firestore.client()
+    doc_ref = store.collection(collection).limit(2)
+    try:
+        docs = doc_ref.stream()
+        for doc in docs:
+            print(type(doc))
+            print(u'Doc Data:{}'.format(doc.to_dict()))
+    except google.cloud.exceptions.NotFound:
+        print(u'Missing data')
+       
+#consultarTodo()
+
+mapa={u'name': u'test', u'added': u'just now'}
+def addFirestoreBD(mapa):
+    store = firestore.client()
+    doc_ref = store.collection(u'test')
+    doc_ref.add(mapa)
+    print("añadido")
+    
+#addFirestoreBD(mapa)
+
+def numRegistros(collection):
+    store = firestore.client()
+    doc_ref = store.collection(collection)
+    data = []
+    docs = doc_ref.stream()
+    for doc in docs:
+        data.append(doc.to_dict())
+    print(len(data))
+    
+#numRegistros(collection)
+
+imagePath = "C:/Users/Felipe Sarmiento/Downloads/buho.jpg"
+nombreArchivo="prueba5"#Como se va guardar en firebase storage
+def addsStorageBD(imagePath,nombreArchivo):
+    storage_client = storage.Client.from_service_account_json("C:/Users/Felipe Sarmiento/Downloads/vitalcol-firebase-adminsdk-ys044-dbc2c8d81d.json")
+    bucket=storage_client.bucket(bucket_name=f"vitalcol.appspot.com")
+    imageBlob = bucket.blob("/")
+    # imagePath = [os.path.join(self.path,f) for f in os.listdir(self.path)]
+    
+    imageBlob = bucket.blob(nombreArchivo)
+    imageBlob.upload_from_filename(imagePath)
+    #imageBlob.upload_from_file(imagePath) Cargar File direcatamente cuando no se tiene ruta
+    url = imageBlob.public_url
+    #output =imageBlob.download_as_string() Capturar Archivo que se subio
+    print(url)
+
+#addImage()
+
+
+
