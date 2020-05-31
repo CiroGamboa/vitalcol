2e3d61e718dd3a61cba3e387195de814ee44f057
Ciro Alberto Gamboa Entralgo <ciro.gamboa.2013@upb.edu.co>
2020-05-30 20:04:00 -0500

    Tesseract workinf

--- tesseract/pruebas.py
+++ tesseract/pruebas.py
@@ -1,15 +1,15 @@
 import cv2 
-#import pytesseract
+import pytesseract
 
 print("PRUEBAS DE TESSERACT\n\n")
 
-#img = cv2.imread('presc-test.jpg')
+img = cv2.imread('presc-test.jpg')
 
 # Adding custom options
-#custom_config = r'--oem 3 --psm 6'
-#pytesseract.image_to_string(img, config=custom_config)
-#print(string)
+custom_config = r'--oem 3 --psm 6'
+string = pytesseract.image_to_string(img, config=custom_config)
+print(string)
 
 #string = pytesseract.image_to_string(img)
 #print(string)
-print("nada")
+print("FUNCIONAAAAA")
